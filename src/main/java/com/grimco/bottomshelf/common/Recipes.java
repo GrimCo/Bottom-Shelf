704ce98744cf5fb9ac5cd31f95932d847d4053a9
mallrat208 <marwat208@gmail.com>
2018-04-05 00:45:35 -0400

    Fixed PGB and Dirty Martini Recipes
    Direct calls to the Item instance resulted in the items missing from the recipes, replaced with Itemstack

--- src/main/java/com/grimco/bottomshelf/common/Recipes.java
+++ src/main/java/com/grimco/bottomshelf/common/Recipes.java
@@ -8,13 +8,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.potion.PotionUtils;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.brewing.BrewingRecipe;
-import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
-import net.minecraftforge.registries.IForgeRegistry;
+
 
 @EventBusSubscriber
 public class Recipes
@@ -33,8 +31,8 @@ public class Recipes
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailDirtyShirley, waterBottle, Items.APPLE, Items.SUGAR, Items.CARROT).setRegistryName("dirtyshirley"));
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailFizzyLifting, waterBottle, Items.GLOWSTONE_DUST, Items.SUGAR, Items.NETHER_WART).setRegistryName("fizzylifting"));
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailMartini, waterBottle, Items.NETHER_WART, Items.BEETROOT).setRegistryName("martini"));
-		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailMartiniDirty, Content.cocktailMartini, Items.GUNPOWDER).setRegistryName("martini_dirty"));
-		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailPGB, Content.bottleJanxSpirit, Items.GOLDEN_APPLE, Items.REDSTONE, Items.BRICK).setRegistryName("pangalacticgargleblaster"));
+		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailMartiniDirty, new ItemStack(Content.cocktailMartini), Items.GUNPOWDER).setRegistryName("martini_dirty"));
+		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.cocktailPGB, new ItemStack(Content.bottleJanxSpirit), Items.GOLDEN_APPLE, Items.REDSTONE, Items.BRICK).setRegistryName("pangalacticgargleblaster"));
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.shotFirewater, waterBottle, Items.BLAZE_POWDER, Items.REDSTONE, Items.NETHER_WART, Items.POTATO).setRegistryName("firewater"));
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.shotRoadkillMalt, waterBottle, Items.NETHER_WART, Items.ROTTEN_FLESH).setRegistryName("roadkillmalt"));
 		event.getRegistry().register(new ShapelessOreRecipe(drinks, Content.jugBrivari, waterBottle, Items.NETHER_WART, Items.BEETROOT).setRegistryName("brivari"));
